5907d8e69a2c180720d60fbdb0d45ee2de718a6a
HM Rando <hrando@smith.edu>
2025-02-18 13:03:09 -0500

    add code

--- app/src/main/java/a1template/CaesarCipher.java
+++ app/src/main/java/a1template/CaesarCipher.java
@@ -4,55 +4,104 @@ package a1template;
 
 public class CaesarCipher {
 
-    /** Character array to store the letters in the alphabet in order */
     Character[] alphabet;
-
-    /** DynamicArray object providing ArrayList-like operations for Characters */
     DynamicArray<Character> cipher;
 
-    /** Private offset that tracks how many positions to shift the index for
-    * This cipher */
+    // Need an offset to keep track of different index mappings
     private int offset;
 
-    /** Constructor that should define the instance variables, including
-     * populating the alphabet
-     * @param offset Offset to use when creating `cipher` of DynamicArray type
-     */
     CaesarCipher(int offset){
         this.offset = offset;
+        this.alphabet = new Character[26];
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = Character.valueOf((char) ('a' + i));
+        }
+       cipher = new DynamicArray<Character>(offset, alphabet);
+       this.offset = offset;
     }
 
-    /** Implementation of linear search that looks through the alphabet
-     * array to identify the position of the passed value
-     * @param val character to search for
-     * @return int indicating position of val in the alphabet array
-     */
+    public char get(int i){
+        if (i-offset < 0){
+            return cipher.get((i - offset) + alphabet.length);
+        } 
+        return cipher.get(i - offset);
+    }
+
     public int findIndex(char val){
-        // This is a stub -- fill in the code and return the
-        // value you calculate
-        return 0;
+        for(int i = 0; i < alphabet.length; i++){
+            if(alphabet[i] == val){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean isLowerCaseCharacter(char c) {
+        return c >= 'a' && c <= 'z';
+    }
+
+    public char encode(int i){
+        int adj = i - offset;
+        char newChar;
+        if(adj >= 0){
+            newChar = cipher.get(adj);
+        } else{
+            newChar = cipher.get(alphabet.length + adj);
+        }
+        System.out.println("    " + adj + ": " + newChar);
+        return newChar;
     }
 
     /** Encode a message using the cipher
      * @param T message to encode
-     * @return encoded message
-     */  
+     * @return encoded message */  
     public String encode(String message){
-        // Fill in here and update return statement based on your code
-        return new String(); 
+        char[] original = message.toCharArray();
+        char[] encoded = new char[original.length];
+        for(int i = 0; i < original.length; i++){
+            Character current = original[i];
+            if(isLowerCaseCharacter(current)){
+                encoded[i] = encode(findIndex(current));
+            } else {
+                encoded[i] = current;
+            }
+        }
+        return new String(encoded); 
      }
+    
+     public char decode(char val){
+        int i = findIndex(val);
+        System.out.println(val + " " + i);
+
+        int adj = i + offset;
+        if (adj >= alphabet.length){
+            adj -= alphabet.length;
+        }
+        return(alphabet[adj]);
+    }
 
     /** Decode a message using the cipher 
      * @param String message to decode
      * @param int key to use in decoding
      * @return decoded message
-     */
+    */
     public String decode(String message){
-        // Fill in here and update return statement based on your code
-        return new String();
+        char[] original = message.toCharArray();
+        char[] decoded = new char[original.length];
+        for(int i = 0; i < original.length; i++){
+            if(isLowerCaseCharacter(original[i])){
+                decoded[i] = decode(original[i]);
+            } else{
+                decoded[i] = original[i];
+            }
+        }
+        return new String(decoded);
     }
 
     public static void main(String[] args) {
-    }
-    
+        CaesarCipher myCipher = new CaesarCipher(3);
+        //System.out.println(myCipher.encode("cressida"));
+        System.out.println(myCipher.decode("zobppfax"));
+        // Challenge: keep upper case
+    }   
 }
